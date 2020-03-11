45b71198232327b2bb2ef946dd272120fb3ce59f
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-03-11 21:19:21 +0200

    Добавьте файлы проекта.

---
+++ LaBA 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaBA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int li = Convert.ToInt32(Math.Pow(age, 2));
+            Console.WriteLine("Число в квадрате  =  ");
+            Console.WriteLine(li);
+            Console.ReadKey();
+        }
+    }
+}
